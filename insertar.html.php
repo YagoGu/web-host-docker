b6e12ffa5bf7f463d4ce0d27e87be381577f4936
ivanferrergarcia <116573204+ivanferrergarcia@users.noreply.github.com>
2023-03-20 15:48:33 +0100

    Add files via upload

---
+++ insertar.html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <link rel="icon" type="image/x-icon" href="Books-icon.ico">
+        <meta charset="utf-8">
+        <title>INSERT</title>
+
+    </head>
+
+    <body>
+        <h1 style="text-align: center"> INSERT YOUR BOOK : </h1>
+    </body>
+    </html>
