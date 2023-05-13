16e54958b1d7838f2a044a1af33a5c5c548d03d1
LettuceFarmer <99267227+LettuceFarmer@users.noreply.github.com>
2023-05-13 16:50:46 +0000

    pepe

--- nginx/website/conexbbd.php
+++ nginx/website/conexbbd.php
@@ -3,7 +3,7 @@ $host = "192.168.0.9";
 $database = "BBDDLibrary";
 $user = "root";
 $password = "password";
-$conexion = mysqli_connect($host, $user, $password,$database);
+$conexion = mysqli_connect($host, $user, $password, $database);
 if (!$conexion)
   die("No ha podido realizarse la conexión".mysqli_connect_error());
 else
